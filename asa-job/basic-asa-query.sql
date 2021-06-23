737e9e661ab25f3b2cd734374394e6e3df784a18
Jurgen Kevelaers <62745957+JurgenOnAzure@users.noreply.github.com>
2021-06-23 20:15:19 +0200

    Update basic-asa-query.sql

--- asa-job/basic-asa-query.sql
+++ asa-job/basic-asa-query.sql
@@ -2,17 +2,12 @@ SELECT
 	System.Timestamp AS EnqueuedUtc,
 	IotHub.ConnectionDeviceId AS DeviceId,
 	Avg(humidity) AS AverageHumidity
-
 INTO
-   bloboutput
-
+  	bloboutput
 FROM
 	iothubinput 
-
 TIMESTAMP BY
 	EventEnqueuedUtcTime
-
 GROUP BY 
 	TumblingWindow(second,15), IotHub.ConnectionDeviceId
-
 HAVING Avg(humidity)>70
